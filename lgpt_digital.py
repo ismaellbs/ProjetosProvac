8197385948190a52a9f2b4761f7f6642abe8d3c6
Ismael Luiz Borges da Silva <ismaellbs@gmail.com>
2024-12-22 22:50:00 -0300

    Versionamento

--- lgpt_digital.py
+++ lgpt_digital.py
@@ -1,13 +1,16 @@
 import streamlit as st
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 import fitz  # PyMuPDF
 from urllib.parse import quote_plus
-senha_codificada = quote_plus("Provac@2024")
-# Substitua pela sua string de conexão do banco de dados
-DATABASE_URI = 'postgresql+psycopg2://admin_provac:{senha_codificada}@192.168.0.232:5432/provac_producao'
 
-# Conexão com o banco
+# Codifique a senha do banco de dados
+senha_codificada = quote_plus("Provac@2024")
+
+# String de conexão com o banco de dados
+DATABASE_URI = f'postgresql+psycopg2://admin_provac:{senha_codificada}@192.168.0.232:5432/provac_producao'
+
+# Função para conectar ao banco de dados
 def get_connection():
     engine = create_engine(DATABASE_URI)
     return engine.connect()
@@ -15,7 +18,7 @@ def get_connection():
 # Função para buscar dados a partir do CPF
 def fetch_user_data(cpf):
     query = f"""
-        SELECT ra_mat, ra_nome, ra_admissao, ra_cc, ct_desccc, ra_depto, r6_deptodesc,foto_url
+        SELECT ra_mat, ra_nome, ra_admissao, ra_cc, ct_desccc, ra_depto, r6_deptodesc
         FROM sua_tabela
         WHERE cpf = '{cpf}'
     """
@@ -39,46 +42,65 @@ def display_pdf(file_path):
         # Exibe a imagem em Streamlit
         st.image(img, caption=f"Página {page_num + 1}")
 
+# Função para inserir dados no banco
+def insert_data(matricula, nome, dt_admissao, cc, desc_cc, depto, desc_depto, foto, termo_aceite):
+    conn = get_connection()
+    query = text("""
+        INSERT INTO rh.aceite_lgpd (matricula, nome, dt_admissao, cc, desc_cc, depto, desc_depto, foto, termo_aceite)
+        VALUES (:matricula, :nome, :dt_admissao, :cc, :desc_cc, :depto, :desc_depto, :foto, :termo_aceite)
+    """)
+    conn.execute(query, matricula=matricula, nome=nome, dt_admissao=dt_admissao, cc=cc, desc_cc=desc_cc, depto=depto, desc_depto=desc_depto, foto=foto, termo_aceite=termo_aceite)
+    conn.close()
 
-# Página de Formulário
-def formulario_page():
-    st.title("Consulta de Dados do Funcionário")
+# Função principal para exibir o formulário e o termo de contrato
+def main_page():
+    st.title("Sistema de Consulta de Funcionários")
 
+    # Formulário de entrada para CPF
     cpf = st.text_input("Insira o CPF do Funcionário")
     if st.button("Consultar"):
         data = fetch_user_data(cpf)
         if not data.empty:
-            st.write("Matrícula:", data['ra_mat'].iloc[0])
-            st.write("Nome:", data['ra_nome'].iloc[0])
-            st.write("Data de Admissão:", data['ra_admissao'].iloc[0])
-            st.write("Centro de Custo:", data['ra_cc'].iloc[0])
-            st.write("Nome do Centro de Custo:", data['ct_desccc'].iloc[0])
-            st.write("Departamento:", data['r6_deptodesc'].iloc[0])
-            st.image(data['foto_url'].iloc[0], caption='Foto do Funcionário')
+            matricula = data['ra_mat'].iloc[0]
+            nome = data['ra_nome'].iloc[0]
+            dt_admissao = data['ra_admissao'].iloc[0]
+            cc = data['ra_cc'].iloc[0]
+            desc_cc = data['ct_desccc'].iloc[0]
+            depto = data['ra_depto'].iloc[0]
+            desc_depto = data['r6_deptodesc'].iloc[0]
+            
+            st.write("Matrícula:", matricula)
+            st.write("Nome:", nome)
+            st.write("Data de Admissão:", dt_admissao)
+            st.write("Centro de Custo:", cc)
+            st.write("Nome do Centro de Custo:", desc_cc)
+            st.write("Departamento:", desc_depto)
+
+            # Upload de Foto do Funcionário
+            st.subheader("Upload da Foto do Funcionário")
+            uploaded_file = st.file_uploader("Tire ou envie uma foto do funcionário", type=["jpg", "jpeg", "png"])
+            foto = None
+            if uploaded_file is not None:
+                foto = uploaded_file.read()
+                st.image(uploaded_file, caption='Foto do Funcionário', use_column_width=True)
+            
+            # Exibição do termo de contrato
+            st.subheader("Termo de Contrato")
+            pdf_file_path = "/Users/macismael/Downloads/Termo LGPD -2024 - Provac.pdf"
+            st.write("Por favor, leia o termo do contrato abaixo:")
+            display_pdf(pdf_file_path)
+            
+            aceite = st.checkbox("Eu aceito os termos do contrato")
+
+            # Botão Enviar
+            if st.button("Enviar"):
+                if aceite and cpf and uploaded_file is not None:
+                    insert_data(matricula, nome, dt_admissao, cc, desc_cc, depto, desc_depto, foto, termo_aceite=aceite)
+                    st.success("Dados enviados com sucesso!")
+                else:
+                    st.error("Por favor, preencha todos os campos obrigatórios: aceite os termos, forneça um CPF e carregue uma foto do funcionário.")
         else:
             st.write("Nenhum dado encontrado para o CPF informado.")
 
-# Página de Termo de Contrato
-def contrato_page():
-    st.title("Termo de Contrato")
-
-    # Caminho para o seu arquivo PDF
-    pdf_file_path = "caminho/para/seu/termo_de_contrato.pdf"
-
-    st.write("Por favor, leia o termo do contrato abaixo:")
-    display_pdf(pdf_file_path)
-    
-    aceite = st.checkbox("Eu aceito os termos do contrato")
-
-    if aceite:
-        st.write("Obrigado por aceitar o termo.")
-
-
-# Configuração das Páginas
-page_names_to_funcs = {
-    "Formulário": formulario_page,
-    "Termo de Contrato": contrato_page,
-}
-
-selected_page = st.sidebar.selectbox("Selecione a página", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page]()
+# Execução da página principal
+main_page()
